02e5bb4d826ba8b5a5ef63793f491b43b0ee367f
sh-andriy <sheredko.andriy@gmail.com>
2022-12-21 01:49:49 +0200

    Weather App - added style to widget's look and forecast, current weather fetch

--- Semester-1/Web/weatherium/src/App.js
+++ Semester-1/Web/weatherium/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import {WEARTHER_API_KEY, WEATHER_API_URL} from "./api";
 
 function App() {
 
     const handleOnSearchChange = (searchData) => {
-        console.log(searchData);
+        const [lat, lon] = searchData.value.split(" ");
+
+        const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEARTHER_API_KEY}`)
+        const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEARTHER_API_KEY}`)
+
     }
 
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange}/>
+        <Search onSearchChange={handleOnSearchChange}/>
+        <CurrentWeather />
     </div>
   );
 }
--- Semester-1/Web/weatherium/src/api.js
+++ Semester-1/Web/weatherium/src/api.js
@@ -8,3 +8,5 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEARTHER_API_KEY = "0955f83b9c579ad43ee0faffa8f18682";
