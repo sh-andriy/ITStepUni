5799156028f7393af25c846944460a92e5fbb4b9
sh-andriy <sheredko.andriy@gmail.com>
2022-12-21 00:56:06 +0200

    Weather App - created react app and adjusted search.js file

---
+++ Semester-1/Web/weatherium/src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '948cddd72bmsh0f88cef1c24d10bp160cddjsn2fa8442230af',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
