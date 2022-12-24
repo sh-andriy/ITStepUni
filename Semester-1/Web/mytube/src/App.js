e980be7a9392733f5d333b1655cb24429c75f794
sh-andriy <sheredko.andriy@gmail.com>
2022-12-24 20:29:29 +0200

    MyTube -made a Youtube clone website on React with MaterialUI

---
+++ Semester-1/Web/mytube/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import './App.css';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
+
+const App = () => (
+    <BrowserRouter>
+      <Box sx={{backgroundColor: "#202124"}}>
+        <Navbar />
+        <Routes>
+            <Route path={"/"} exact element={<Feed/>}/>
+            <Route path={"/video/:id"} element={<VideoDetail/>}/>
+            <Route path={"/chanel/:id"} element={<ChannelDetail/>}/>
+            <Route path={"/search/:searchTerm"} element={<SearchFeed/>}/>
+        </Routes>
+      </Box>
+    </BrowserRouter>
+);
+
+export default App;
---
+++ Semester-1/Web/mytube/src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = '../1280px-YouTube_Premium_logo.svg.png';
+
+export const categories = [
+    { name: 'New', icon: <HomeIcon />, },
+    { name: 'Lofi Girl', icon: <CodeIcon />, },
+    { name: 'Coding', icon: <CodeIcon />, },
+    { name: 'Linus Tech Tips', icon: <CodeIcon />, },
+    { name: 'Tech With Tim', icon: <CodeIcon />, },
+    { name: 'Music', icon: <MusicNoteIcon /> },
+    { name: 'Education', icon: <SchoolIcon />, },
+    { name: 'Podcast', icon: <GraphicEqIcon />, },
+    { name: 'Movie', icon: <OndemandVideoIcon />, },
+    { name: 'Gaming', icon: <SportsEsportsIcon />, },
+    { name: 'Live', icon: <LiveTvIcon />, },
+    { name: 'Sport', icon: <FitnessCenterIcon />, },
+    { name: 'Fashion', icon: <CheckroomIcon />, },
+    { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+    { name: 'Comedy', icon: <TheaterComedyIcon />, },
+    { name: 'Gym', icon: <FitnessCenterIcon />, },
+    { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ytimg.com/vi/jfKfPfyJRdk/hqdefault_live.%E2%80%A6AFwAcABBg==&rs=AOn4CLD0rgRIyMq2Sxst3s4KbG-a3dNd3A';
+export const demoChannelUrl = 'https://www.youtube.com/@LofiGirl';
+export const demoVideoUrl = 'https://www.youtube.com/watch?v=jfKfPfyJRdk&ab_channel=LofiGirl';
+export const demoChannelTitle = 'Lofi Girl';
+export const demoVideoTitle = 'lofi hip hop radio - beats to relax/study to';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
